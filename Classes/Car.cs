d397b95be38044791fe879a835bf70927b01a442
tyson lind <tysonfloydlind@gmail.com>
2024-01-19 20:26:16 -0700

    completed classes exercise 1

---
+++ Classes/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public string Make;
+        public string Model;
+        public int Year;
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Classes
+﻿
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Car roadster = new Car("BMW Z4", "Roadster", 2024);
+            Console.WriteLine($"{roadster.Make}\n{roadster.Model}\n{roadster.Year}");
         }
     }
 }
